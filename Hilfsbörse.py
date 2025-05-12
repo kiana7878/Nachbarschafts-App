3390008453d7f85d63a40bff6559d36ab48e75c3
kiana7878 <kiana.afzal@gmx.de>
2025-05-12 12:14:02 +0200

    Update Hilfsbörse.py

--- Hilfsbörse.py
+++ Hilfsbörse.py
@@ -3,4 +3,5 @@ print("Wilkommen bei der Hilfsbörse bitte geben sie einfach einen Satz ein, wob
 eingabe = input("Wobei brauchen sie Hilfe? :")
 
 # ich muss das so machen dass es eine liste gibt in der alle nachbaren sind und dann eine zählschleife gibt die nachbarn mal die nachricht an die nachbarn weitergibt
-#
+#die nachricht musst individuell geschickt werden können in der zweiten stuffe wenn man zum bsp beim einkaufen hilfe braucht werden nur leute unter 30 angefragt
+# kann man mit namen speichern und lesen lassen und nur dennen wird geschickt
