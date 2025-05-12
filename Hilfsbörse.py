a9453d455222e402dcd55a808cdcdd5fadec1756
kiana7878 <kiana.afzal@gmx.de>
2025-05-12 12:10:16 +0200

    Create Hilfsbörse.py

---
+++ Hilfsbörse.py
@@ -0,0 +1,6 @@
+#Nachbarschafts-App: Hilfsbörse
+print("Wilkommen bei der Hilfsbörse bitte geben sie einfach einen Satz ein, wobei Sie Hilfe brauchen.")
+eingabe = input("Wobei brauchen sie Hilfe? :")
+
+# ich muss das so machen dass es eine liste gibt in der alle nachbaren sind und dann eine zählschleife gibt die nachbarn mal die nachricht an die nachbarn weitergibt
+#
