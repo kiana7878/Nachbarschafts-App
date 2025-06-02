e89be1cb401968a0e9cb1d7f49bf71522f2048ab
kiana7878 <kiana.afzal@gmx.de>
2025-06-02 11:59:38 +0200

    Update Hilfsbörse.py

--- Hilfsbörse.py
+++ Hilfsbörse.py
@@ -1,16 +1,16 @@
 #Nachbarschafts-App: Hilfsbörse
-print("Willkommen bei der Hilfsbörse! Bitte schreiben Sie einfach ein Wort bei dem Sie Hilfe brauchen und Ihren Namen.")
-eingabe = input("Wobei brauchen Sie Hilfe? :")
+print("Willkommen bei der Hilfsbörse! Bitte schreiben Sie einfach ein Wort bei dem Sie Hilfe brauchen und Ihren Namen.") #Eine kleine Anleitung zum benutzen
+eingabe = input("Wobei brauchen Sie Hilfe? :") #Es wird gefragt wobei man Hilfe braucht und wie man heißt.
 name = input("Wie heißen Sie? :")
 
-nachbarn = ["Tom", "Lilly", "Anabell", "Max", "Bernd", "Greta"]
+nachbarn = ["Tom", "Lilly", "Anabell", "Max", "Bernd", "Greta"] #Das ist die Liste der Nachbarn.
 for i in range(0, len(nachbarn)):
     print("Liebe/-r " + nachbarn[i] + ",op " + name + " braucht ihre Hilfe bei/-m " + eingabe)
-    
+# In dieser Schleife wird pro Nachbar in der Liste eine Nachricht abgesendet die, die Frage beeinhaltet ob man Helfen möchte.   
     
 for a in range(0, len(nachbarn)):    
-    print("Wenn Sie helfen wollen antworten Sie mit Ja, wenn nicht mit Nein.")
-    antwort = input("Wollen Sie helfen? :")
+    print("Wenn Sie helfen wollen antworten Sie mit Ja, wenn nicht mit Nein.") #Wieder eine kleine erklärung was man machen soll.
+    antwort = input("Wollen Sie helfen? :")# Die Frage wird gestellt
     
     if antwort == "Ja" or antwort == "ja":
         print(nachbarn [a] + " will helfen.")
@@ -18,3 +18,4 @@ for a in range(0, len(nachbarn)):
         print(nachbarn [a] + " will nicht helfen.")
     else:
         print("Ungültige Antwort.")
+# In dieser Bedingten Anweisund wir kontroliert, welche Antwort man gegeben hat und je nach Antwort eine Aussage ausgegeben.
